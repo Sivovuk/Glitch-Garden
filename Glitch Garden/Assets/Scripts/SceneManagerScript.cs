6156d6be2c43471374304bd288eaa107b5221bc6
ZimskiVuk <thepackgamestudio@gmail.com>
2020-08-26 09:33:55 +0200

    ...

---
+++ Glitch Garden/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    private static SceneManagerScript instance;
+
+    public static SceneManagerScript Instance => instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void RestartScene() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+	public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
